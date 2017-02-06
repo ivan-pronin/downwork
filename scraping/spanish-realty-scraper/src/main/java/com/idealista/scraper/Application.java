397cbb1e8df104b3080f7b256f95e4cf7cb049ae
Ivan Pronin <Ivan Pronin@epam.com>
2017-02-06 18:39:27 +0300

    Add /spanish-realty-scraper project

---
+++ scraping/spanish-realty-scraper/src/main/java/com/idealista/scraper/Application.java
@@ -0,0 +1,10 @@
+package com.idealista.scraper;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        String baseUrl = "https://www.idealista.com/venta-viviendas/a-coruna-provincia/";
+        
+    }
+}
