2a7e14b116ee3abef71f806195c5d3fdab181530
Ivan Pronin <ivan_pronin@epam.com>
2017-12-03 21:50:11 +0300

    REALTY-DB: wip add simple db unit-test

--- scraping/realty-scraper-web/src/test/java/com/idealista/db/DBTest.java
+++ scraping/realty-scraper-web/src/test/java/com/idealista/db/DBTest.java
@@ -2,8 +2,6 @@ package com.idealista.db;
 
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +10,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.idealista.scraper.AppConfig;
 import com.idealista.web.Boot;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Boot.class, AppConfig.class})
+@SpringBootTest(classes = Boot.class)
 @TestPropertySource(locations = "classpath:test.properties")
 public class DBTest
 {
@@ -24,13 +21,10 @@ public class DBTest
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private DataSource dataSource;
-
     @Test
     public void testConnection() throws SQLException
     {
-        System.out.println("Metadata: " + dataSource.getConnection().getMetaData());
+        System.out.println("Metadata: " + jdbcTemplate.getDataSource().getConnection().getMetaData());
         int rowCount = jdbcTemplate.queryForObject("select count(*) from scraper.agency", Integer.class);
         System.out.println("Rowcount: " + rowCount);
     }
